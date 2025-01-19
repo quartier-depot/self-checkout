7f61c035397d6c5a17dc5ccaa5bd764cc0856a29
Christian Abegg <christian.abegg@zuehlke.com>
2025-01-19 17:40:03 +0100

    typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,13 @@
-import "./App.css";
+import './App.css'
 
 function App() {
-
   return (
-      <main className="container">
+      <>
           <h1 className="text-3xl font-bold underline">
               Hello world!
           </h1>
-      </main>
-  );
+      </>
+  )
 }
 
-export default App;
+export default App
