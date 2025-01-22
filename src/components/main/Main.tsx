752fcf9a51403917bf314a5fca5b779a96003faf
Christian Abegg <christian.abegg@zuehlke.com>
2025-01-22 20:48:01 +0100

    align background

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -9,13 +9,13 @@ export function Main() {
 
     return (
         <>
-            <div className={'bg-blue-gray-50 hide-print flex flex-row h-screen antialiased text-blue-gray-800'}>
+            <div className={'hide-print flex flex-row h-screen antialiased text-blue-gray-800'}>
                 <div className={'flex-grow flex'}>
                     <div className={'flex flex-col bg-slate-50 h-full w-full py-4'}>
                         <Search />
                         <Products />
                     </div>
-                    <div className={'w-5/12 flex flex-col bg-blue-gray-50 h-full bg-white pr-4 pl-2 py-4'}>
+                    <div className={'w-5/12 flex flex-col bg-slate-50 h-full pr-4 pl-2 py-4'}>
                         <div className={'bg-white rounded-3xl flex flex-col h-full shadow'}>
                             <Cart/>
                             <Payment />
