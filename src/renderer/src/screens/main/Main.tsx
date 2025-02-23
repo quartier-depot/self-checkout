91756cc68e96fc161efe220ed064c80541142468
Christian Abegg <christian.abegg@zuehlke.com>
2025-02-23 10:09:04 +0100

    extract barcode component

--- src/renderer/src/screens/main/Main.tsx
+++ src/renderer/src/screens/main/Main.tsx
@@ -6,15 +6,12 @@ import { Products } from './products/Products';
 import { Cart } from './cart/Cart';
 import { Payment } from './payment/Payment';
 import { Loading } from '../../components/modal/loading/Loading';
-import KeyboardBarcodeScanner from '../../external/@point-of-sale/keyboard-barcode-scanner/main';
 import { getConfiguration } from '../../configuration/getConfiguration';
 import classNames from 'classnames';
 import barcode from '../../assets/barcode.svg';
 import { Customer } from './customer/Customer';
-import { Product } from '../../api/products/Product';
-import { setCustomer } from '../../state/customer/setCustomer';
-import { changeCartQuantity } from '../../state/cart/changeCartQuantity';
 import { CustomerActionTypes } from '../../state/customer/customerAction';
+import { Barcode } from './barcode/Barcode';
 
 export function Main() {
   const { dispatch } = useAppContext();
@@ -22,113 +19,9 @@ export function Main() {
   const customersQuery = useCustomers();
   const configuration = getConfiguration();
 
-  async function setupScanner() {
-    const keyboardScanner = new KeyboardBarcodeScanner();
-    keyboardScanner.addEventListener('connected', () => {
-      console.log(`Connected to barcode scanner(s) in keyboard emulation mode.`);
-    });
-    keyboardScanner.addEventListener('disconnected', () => {
-      console.log(`Disconnected from barcode scanner(s) in keyboard emulation mode.`);
-    });
-    keyboardScanner.connect();
-    keyboardScanner.addEventListener('barcode', handleBarcodeEvent);
-  }
-
-  function handleBarcodeEvent(e: BarcodeEvent) {
-    if (!(e.value)) {
-      return;
-    }
-    if (e.value.startsWith('qdm')) {
-      memberInput(e.value);
-    } else {
-      barcodeInput(e);
-    }
-  }
-
-  function memberInput(barcode: string) {
-    const memberId = barcode.substring('qdm'.length).replaceAll('\'', '-');
-    if (!customersQuery.data) {
-      return;
-    }
-
-    const customer = customersQuery.data.find((customer) => customer.member_id === memberId);
-    if (customer) {
-      dispatch(setCustomer(customer));
-    } else {
-      console.log('No customer found with memberId ' + barcode);
-
-    }
-  }
-
-  function barcodeInput(barcodeEvent: BarcodeEvent) {
-    if (!productsQuery.data) {
-      return;
-    }
-
-    let products: Product[] = [];
-    switch (barcodeEvent.symbology) {
-      case 'qr-code':
-        const id = getProductId(barcodeEvent.value);
-        if (id) {
-          products = productsQuery.data.filter((product: Product) => {
-            return product.id === id;
-          });
-          break;
-        }
-
-        const slug = getProductSlug(barcodeEvent.value);
-        if (slug) {
-          products = productsQuery.data.filter((product: Product) => {
-            return product.slug === slug;
-          });
-          break;
-        }
-
-        console.error('Unknown QR code for product search: ' + barcodeEvent.value);
-        break;
-
-      default:
-        products = productsQuery.data.filter((product: Product) => {
-          return product.barcode === barcodeEvent.value;
-        });
-    }
-
-    switch (products.length) {
-      case 0:
-        console.log('Nothing found for ' + barcodeEvent.value);
-        break;
-      case 1:
-        dispatch(changeCartQuantity(1, products[0]));
-        break;
-      default:
-        console.log('Found multiple found for ' + barcodeEvent.value);
-        break;
-    }
-  }
-
-  function getProductSlug(url: String) {
-    const regex = /.*\/produkt\/(.*)/;
-    const match = url.match(regex);
-    if (match) {
-      return match[1].replace(/\//g, '');
-    }
-    return undefined;
-  }
-
-  function getProductId(url: String) {
-    const regex = /.*\/?p=(\d*)/;
-    const match = url.match(regex);
-    if (match) {
-      return Number.parseInt(match[1]);
-    }
-    return undefined;
-  }
-
-
   function loginUser() {
     dispatch({ type: CustomerActionTypes.SET_CUSTOMER, payload: customersQuery.data?.find(c => c.id === 213) });
   }
-
 
   const loadingData = !productsQuery.isSuccess || !customersQuery.isSuccess;
 
@@ -150,13 +43,6 @@ export function Main() {
           <div className={'absolute bottom-10 left-8'}>
             <button type="button"
                     id="scanner-button"
-                    onClick={setupScanner}
-                    className={classNames('bg-amber-200 p-1', { 'hidden': configuration.electron })}>
-              <img src={barcode} alt="barcode" className={'h-6 w-6 inline-block'} />
-            </button>
-
-            <button type="button"
-                    id="scanner-button"
                     onClick={loginUser}
                     className={classNames('bg-amber-200 p-1', { 'hidden': configuration.electron })}>
               <img src={barcode} alt="user" className={'h-6 w-6 inline-block'} />
@@ -166,6 +52,7 @@ export function Main() {
       </div>
 
       {loadingData && <Loading />}
+      <Barcode />
     </>
   );
 }
