6d8fe46ade5d018abddb607e2835a4d947aa6148
Christian Abegg <christian.abegg@zuehlke.com>
2025-03-08 16:50:20 +0100

    add express to snap

--- bin/start-webserver.sh
+++ bin/start-webserver.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Change to the web directory
-cd $SNAP/dist
+cd $SNAP
 
 # Start the http-server
-$SNAP/bin/node $SNAP/lib/node_modules/quartier-depot-self-checkout/node_modules/.bin/http-server -p 3000
+$SNAP/bin/node $SNAP/lib/node_modules/quartier-depot-self-checkout/node_modules/.bin/express express.cjs
