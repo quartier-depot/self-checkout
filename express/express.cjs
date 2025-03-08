00b4b20612faf93dae20bb37ab931fb1512b68d6
Christian Abegg <christian.abegg@zuehlke.com>
2025-03-08 21:55:57 +0100

    move express in dedicated folder

--- express/express.cjs
+++ express/express.cjs
@@ -4,30 +4,25 @@ const path = require('path');
 const dotenv = require('dotenv');
 
 // Load environment variables from .env file
-dotenv.config();
+dotenv.config({path: path.join(__dirname, '..', '.env')});
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Serve static files from the dist folder
-app.use(express.static(path.join(__dirname, 'dist')));
+app.use(express.static(path.join(__dirname, '..', 'dist')));
 
 // Proxy /wp-json calls to the target server
 app.use('/wp-json', createProxyMiddleware({
   target: `${process.env.VITE_WOOCOMMERCE_URL}/wp-json`,
   changeOrigin: true,
   secure: false,
-  auth: `${process.env.VITE_WOOCOMMERCE_CONSUMER_KEY}:${process.env.VITE_WOOCOMMERCE_CONSUMER_SECRET}`,
-  on: {
-    proxyReq: (proxyReq, req, res) => {
-      console.log("proxy request"+proxyReq.host+proxyReq.path);
-    }
-  }
+  auth: `${process.env.VITE_WOOCOMMERCE_CONSUMER_KEY}:${process.env.VITE_WOOCOMMERCE_CONSUMER_SECRET}`
 }));
 
 // Fallback to index.html for SPA
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'dist', 'index.html'));
 });
 
 app.listen(PORT, () => {
