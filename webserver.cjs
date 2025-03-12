0f71933a97e0c5e576ea683c986b3d18435844a9
Christian Abegg <christian.abegg@zuehlke.com>
2025-03-12 13:03:13 +0100

    change order of tracking setup

--- webserver.cjs
+++ webserver.cjs
@@ -1,46 +1,39 @@
-const express = require('express');
-const { createProxyMiddleware } = require('http-proxy-middleware');
-const path = require('path');
-const dotenv = require('dotenv');
 const { useAzureMonitor } = require("@azure/monitor-opentelemetry");
 const { NodeTracerProvider } = require('@opentelemetry/sdk-trace-node');
 const { registerInstrumentations } = require('@opentelemetry/instrumentation');
 const { HttpInstrumentation } = require('@opentelemetry/instrumentation-http');
 const { ExpressInstrumentation } = require('@opentelemetry/instrumentation-express');
+const path = require('path');
 
-const snap = process.env.SNAP;
-
+// configure
 let root = "";
 let config = {
   woocommerce: {},
   applicationInsights: {}
 };
 
+const snap = process.env.SNAP;
 if (snap) {   // executed in snap environment
   root = path.join(snap, 'dist');
 } else { // executed in development environment
   root = path.join(__dirname, '..', 'dist');
+  const dotenv = require('dotenv');
   dotenv.config({path: path.join(__dirname, '..', '.env')});
 }
 
-// set config
 config.woocommerce.url = process.env.WOOCOMMERCE_URL || process.env.VITE_WOOCOMMERCE_URL;
 config.woocommerce.consumerKey = process.env.WOOCOMMERCE_CONSUMER_KEY || process.env.VITE_WOOCOMMERCE_CONSUMER_KEY;
 config.woocommerce.consumerSecret = process.env.WOOCOMMERCE_CONSUMER_SECRET || process.env.VITE_WOOCOMMERCE_CONSUMER_SECRE;
 config.applicationInsights.connectionString = process.env.APPLICATIONINSIGHTS_CONNECTION_STRING || process.env.VITE_APPLICATIONINSIGHTS_CONNECTION_STRING;
 
 
-// setup open telemetry
+// tracking
 const options = {
   azureMonitorExporterOptions: {
     connectionString: config.applicationInsights.connectionString
   },
 }
 useAzureMonitor(options);
-
-const provider = new NodeTracerProvider();
-provider.register();
-
 registerInstrumentations({
   instrumentations: [
     // Express instrumentation expects HTTP layer to be instrumented
@@ -49,8 +42,11 @@ registerInstrumentations({
   ],
 });
 
+// express
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 // Serve static files from the dist folder
 app.use(express.static(root));
@@ -73,6 +69,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(root, 'index.html'));
 });
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
