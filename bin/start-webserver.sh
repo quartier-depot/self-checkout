29788f1135cc6576dcfe4252f162afe1da75a1ec
Christian Abegg <christian.abegg@zuehlke.com>
2025-03-12 12:59:47 +0100

    export environment variables

--- bin/start-webserver.sh
+++ bin/start-webserver.sh
@@ -4,9 +4,9 @@ set -e
 URL=$(snapctl get url)
 API_KEY=$(snapctl get api-key)
 
-WOOCOMMERCE_URL=$(snapctl get woocommerce-url)
-WOOCOMMERCE_CONSUMER_KEY=$(snapctl get woocommerce-consumer-key)
-WOOCOMMERCE_CONSUMER_SECRET=$(snapctl get woocommerce-consumer-secret)
-APPLICATIONINSIGHTS_CONNECTION_STRING=$(snapctl get applicationinsights-connection-string)
+export WOOCOMMERCE_URL=$(snapctl get woocommerce-url)
+export WOOCOMMERCE_CONSUMER_KEY=$(snapctl get woocommerce-consumer-key)
+export WOOCOMMERCE_CONSUMER_SECRET=$(snapctl get woocommerce-consumer-secret)
+export APPLICATIONINSIGHTS_CONNECTION_STRING=$(snapctl get applicationinsights-connection-string)
 
 $SNAP/bin/node $SNAP/lib/node_modules/self-checkout/webserver.cjs
