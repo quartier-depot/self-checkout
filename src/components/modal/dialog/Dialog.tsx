aa84f1df05398d525f6bfe05747d4de0f090f7b2
Christian Abegg <christian.abegg@zuehlke.com>
2025-04-25 17:23:45 +0200

    optional title

--- src/components/modal/dialog/Dialog.tsx
+++ src/components/modal/dialog/Dialog.tsx
@@ -2,19 +2,21 @@ import { Modal } from '../Modal';
 import { PropsWithChildren } from 'react';
 
 type DialogProps = PropsWithChildren<{
-    title: string
+    title?: string
 }>;
 
 
 export function Dialog(props: DialogProps) {
     return (
-            <Modal>
-                <div className={'bg-slate-50 rounded-lg w-3/4 h-3/4 scale-95 transition-transform duration-300 ease-out'}>
+        <Modal>
+            <div className={'bg-slate-50 rounded-lg w-3/4 h-3/4 scale-95 transition-transform duration-300 ease-out'}>
+                {props.title &&
                     <div className="border-b border-stone-200 p-4 flex justify-between items-center">
                         <h1 className="text-lg text-blue-gray-800 font-semibold">{props.title}</h1>
                     </div>
-                    {props.children}
-                </div>
-            </Modal>
+                }
+                {props.children}
+            </div>
+        </Modal>
     );
 }
