2dd66a2fdd08a646c5776ebe8d1db38cfd6a443e
Christian Abegg <christian.abegg@zuehlke.com>
2025-04-25 08:01:04 +0200

    fix error if there is no meta_data

--- src/api/getMetaData.ts
+++ src/api/getMetaData.ts
@@ -1,4 +1,8 @@
 export function getMetaData(key: string, dto: any): string {
+  if (!dto || !dto.meta_data || !Array.isArray(dto.meta_data)) {
+    return '';
+  }
+
   return (
     dto.meta_data.find((meta: { key: string; value: string }) => meta.key === key)?.value ?? ''
   );
