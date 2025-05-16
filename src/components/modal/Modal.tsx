a5ee26c41db1592f7aa6d1191f894e010a943cf3
Christian Abegg <christian.abegg@zuehlke.com>
2025-05-16 17:50:02 +0200

    rename to onBackdropClick

--- src/components/modal/Modal.tsx
+++ src/components/modal/Modal.tsx
@@ -1,14 +1,22 @@
 import { PropsWithChildren } from 'react';
 
 type ModalProps = {
-    onClick?: () => void;
+    onBackdropClick?: () => void;
 }
 
-export function Modal({ onClick, children }: PropsWithChildren<ModalProps>) {
+export function Modal({ onBackdropClick, children }: PropsWithChildren<ModalProps>) {
     return (
-            <div className="h-screen w-full fixed left-0 top-0 flex justify-center items-center bg-slate-950 z-10"
-                 onClick={onClick}>
+        <div className="h-screen w-full fixed left-0 top-0 flex justify-center items-center z-10">
+            <div 
+                className="absolute inset-0 bg-slate-950"
+                onClick={onBackdropClick}
+            />
+            <div 
+                className="relative z-20 w-full h-full flex justify-center items-center"
+                onClick={(e) => e.stopPropagation()}
+            >
                 {children}
             </div>
+        </div>
     );
 }
--- src/components/modal/dialog/Dialog.tsx
+++ src/components/modal/dialog/Dialog.tsx
@@ -6,10 +6,9 @@ type DialogProps = PropsWithChildren<{
     onClick?: () => void
 }>;
 
-
 export function Dialog(props: DialogProps) {
     return (
-        <Modal onClick={props.onClick}>
+        <Modal onBackdropClick={props.onClick}>
             <div className={'bg-slate-50 rounded-lg w-3/4 h-3/4 scale-95 transition-transform duration-300 ease-out flex flex-col'}>
                 {props.title &&
                     <div className="border-b border-stone-200 p-4 items-center">
