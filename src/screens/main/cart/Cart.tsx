2d64a46ab8bd620aead9e7a9ab00168a60a72ec4
Christian Abegg <christian.abegg@zuehlke.com>
2025-05-16 18:42:33 +0200

    add cart dialog to restart

--- src/screens/main/cart/Cart.tsx
+++ src/screens/main/cart/Cart.tsx
@@ -7,6 +7,8 @@ import { NumPad } from './numPad/NumPad';
 import { Dialog } from '../../../components/modal/dialog/Dialog';
 import { setCartQuantity } from '../../../state/cart/setCartQuantity';
 import { useAppInsightsContext } from '@microsoft/applicationinsights-react-js';
+import { Button } from '../../../components/button/Button';
+import { emptyCart } from '../../../state/cart/emptyCart';
 
 export function Cart() {
     const { state } = useAppContext();
@@ -14,6 +16,7 @@ export function Cart() {
     const applicationInsights = useAppInsightsContext();
 
     const [dialogItem, setDialogItem] = useState<ItemType | null>(null);
+    const [showCartDialog, setShowCartDialog] = useState(false);
 
     function openDialog(item: ItemType): void {
         setDialogItem(item);
@@ -28,14 +31,23 @@ export function Cart() {
         applicationInsights.getAppInsights().trackEvent({ name: 'product-error' }, { location: 'cart', product: item.product.artikel_id });
     }
 
+    function restartCart() {
+        dispatch(emptyCart());
+    }
+
+    function restartApplication() {
+        window.location.reload();
+    }
+
     return (
         <>
             <div className={'h-12 text-left flex'}>
                 <div className={'pl-2 py-2 relative'}>
                     <img src={cart} alt="cart" className={'h-6 inline-block'} />
                     <div
+                        onClick={() => setShowCartDialog(true)}
                         className={
-                            'text-center absolute bg-emerald-700 text-white w-5 h-5 text-xs p-0 leading-5 rounded-full -right-2 top-3'
+                            'text-center absolute bg-emerald-700 text-white w-5 h-5 text-xs p-0 leading-5 rounded-full -right-2 top-3 cursor-pointer'
                         }
                     >
                         {state.cart.quantity}
@@ -45,14 +57,12 @@ export function Cart() {
 
             <div className={'overflow-y-auto overflow-x-hidden flex-1 font-mono'}>
                 <table className={'table-fixed w-full'}>
-
                     <colgroup>
                         <col className="w-[45px]" />
                         <col className="truncate" />
                         <col className="w-1/6" />
                     </colgroup>
                     <tbody>
-
                         {state.cart.items.map((item) => (
                             <Item key={item.product.id} item={item} onClick={() => openDialog(item)} />
                         ))}
@@ -66,6 +76,18 @@ export function Cart() {
                 onChange={(quantity) => closeDialog(quantity)}
                 onReportError={() => reportError(dialogItem)} />
             </Dialog>}
+
+            {showCartDialog && (
+                <Dialog title="Was möchtest du tun?" onBackdropClick={() => setShowCartDialog(false)}>
+                    <div className="p-4">
+                        <div className="grid grid-cols-2 gap-4">
+                            <Button type="secondary" onClick={restartCart} className="aspect-square"><b>Einkauf</b><br />neu starten</Button>
+                            <Button type="secondary" onClick={restartApplication} className="aspect-square"><b>Kasse</b><br />neu starten</Button>
+                        </div>
+                        <Button type="primary" onClick={() => setShowCartDialog(false)} className="mt-4">Abbrechen</Button>
+                    </div>
+                </Dialog>
+            )}
         </>
     );
 }
