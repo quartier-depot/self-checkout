5eb620e8b76040cd824638c6e68754bd78997013
Christian Abegg <christian.abegg@zuehlke.com>
2025-06-14 15:10:09 +0200

    extract snap version

--- bin/start-webserver.sh
+++ bin/start-webserver.sh
@@ -5,5 +5,6 @@ export WOOCOMMERCE_URL=$(snapctl get woocommerce-url)
 export WOOCOMMERCE_CONSUMER_KEY=$(snapctl get woocommerce-consumer-key)
 export WOOCOMMERCE_CONSUMER_SECRET=$(snapctl get woocommerce-consumer-secret)
 export APPLICATIONINSIGHTS_CONNECTION_STRING=$(snapctl get applicationinsights-connection-string)
+export SNAP_VERSION=$(snap info quartier-depot-self-checkout | grep "^installed:" | awk '{print $2}')
 
 $SNAP/bin/node $SNAP/lib/node_modules/self-checkout/webserver.cjs
