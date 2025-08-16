816a65903f0246ff8690279e21195f652173f8d7
Christian Abegg <christian.abegg@zuehlke.com>
2025-08-16 15:14:19 +0200

    auto-close confirmation dialog

--- app/src/screens/main/payment/confirmation/Confirmation.tsx
+++ app/src/screens/main/payment/confirmation/Confirmation.tsx
@@ -2,6 +2,7 @@ import { formatPrice } from '../../../../format/formatPrice';
 import { Dialog } from '../../../../components/modal/dialog/Dialog';
 import { Button } from '../../../../components/button/Button';
 import cartCheckIcon from '../../../../assets/cart-check.svg';
+import { useEffect } from 'react';
 
 type Props = {
     total: number,
@@ -12,6 +13,14 @@ type Props = {
 }
 
 export function Confirmation(props: Props) {
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            props.onClose();
+        }, 60000); // 60 seconds
+
+        return () => clearTimeout(timer);
+    }, [props.onClose]);
+
     return (
             <Dialog title={'Einkauf erfolgreich'}>
                 <div className="p-4 text-blue-gray-800">
@@ -20,10 +29,14 @@ export function Confirmation(props: Props) {
                         <img src={cartCheckIcon} alt="success" className={'h-24 inline-block'} />
                     </p>
                     <div className={'grid grid-cols-2 gap-2 my-4'}>
-                        <div>Betrag:</div><span className="font-bold text-right">{formatPrice(props.total)}</span>
-                        <div>Virtueller Kontostand neu:</div>  <span className="text-right">{formatPrice(props.newBalance)}</span>
-                        <div>Bestellnummer:</div> <span className="text-right">{props.orderId}</span>
-                        <div>Transaktionsnummer:</div><span className="text-right">{props.transactionId}</span>
+                        <div>Betrag:</div>
+                        <span className="font-bold text-right">{formatPrice(props.total)}</span>
+                        <div>Virtueller Kontostand neu:</div>
+                        <span className="text-right">{formatPrice(props.newBalance)}</span>
+                        <div>Bestellnummer:</div>
+                        <span className="text-right">{props.orderId}</span>
+                        <div>Transaktionsnummer:</div>
+                        <span className="text-right">{props.transactionId}</span>
                     </div>
                     <Button type="primary" onClick={() => props.onClose()}>Schliessen</Button>
                 </div>
