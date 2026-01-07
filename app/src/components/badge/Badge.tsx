9d20449b3731e45ff2ee0a20037a53127babe666
Christian Abegg <christian.abegg@zuehlke.com>
2026-01-07 16:53:46 +0100

    lowercase testid

--- app/src/components/badge/Badge.tsx
+++ app/src/components/badge/Badge.tsx
@@ -7,5 +7,5 @@ type BadgeProps = PropsWithChildren<{
 
 export function Badge({ children, className, testId}: BadgeProps) {
     return (
-            <span className={className +" bg-emerald-800 text-white px-2 py-1 text-xs font-bold rounded-full"} data-testId={testId || ''}>{children}</span>);
+            <span className={className +" bg-emerald-800 text-white px-2 py-1 text-xs font-bold rounded-full"} data-testid={testId || ''}>{children}</span>);
 }
