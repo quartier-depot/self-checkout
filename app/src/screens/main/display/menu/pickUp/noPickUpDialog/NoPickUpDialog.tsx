2c3a6903403a1b6bec4d573e1bd31715f6edc539
Christian Abegg <christian.abegg@zuehlke.com>
2026-01-12 19:35:07 +0100

    nicer no pre-order dialog

--- app/src/screens/main/display/menu/pickUp/noPickUpDialog/NoPickUpDialog.tsx
+++ app/src/screens/main/display/menu/pickUp/noPickUpDialog/NoPickUpDialog.tsx
@@ -1,5 +1,6 @@
 import { Dialog } from '../../../../../../components/modal/dialog/Dialog.tsx';
 import { Button } from '../../../../../../components/button/Button.tsx';
+import { QRCodeSVG } from 'qrcode.react';
 
 
 type NoAboDialogProps = {
@@ -8,17 +9,26 @@ type NoAboDialogProps = {
 
 export function NoPickUpDialog({ onClose }: NoAboDialogProps) {
     return (
-            
-            <Dialog title="Interessiert am Abo?" onBackdropClick={onClose}>
-                <div className={'p-4'} onClick={onClose}>
-                    Unsere Frischwaren (Gemüse, Früchte, Milch, Butter, Joghurt) kommen im Abo. 
-                    Dieses kann alle ein, zwei oder drei Wochen bestellt werden und lässt sich  pausieren.
-                    Auf der Webseite erfährst du alles über die Frischwaren Abos und kannst sie sogleich bestellen.
-                </div>
-                <div className={'p-4'} onClick={onClose}>
-                    Hast du bereits ein Abo? Dann liegt möglicherweise ein Fehler vor,
-                    denn deine Abo Bestellung dieser Woche sollte hier angezeigt werden.
-                    Bitte melde dich bei uns.
+            <Dialog title="Keine Vorbestellung gefunden" onBackdropClick={onClose} className={'size-7/8'} >
+                <div className={'flex-1 grid grid-cols-2 grid-rows-1 gap-4 overflow-y-auto'} onClick={onClose}>
+                    <div>
+                        <div className={'p-4'}>
+                            Unsere Frischwaren (Gemüse, Früchte, Milch, Butter, Joghurt) kommen im Abo.
+                            Dieses kann alle ein, zwei oder drei Wochen bestellt werden und lässt sich  pausieren.
+                            Auf der Webseite (QR-Code links) erfährst du alles über die Frischwaren Abos und kannst sie sogleich bestellen.
+                        </div>
+                        <div className={'p-4'} >
+                            Aktuell hast du keine Vorbestellung.
+                            Vorbestellungen erscheinen in der Kasse bis 5 Tage nach dem Abholtag.
+                        </div>
+                    </div>
+                    <div>
+                        <div className={'p-4'}>
+                            <div className={'flex justify-center items-center'}>
+                                <QRCodeSVG value="https://webshop.quartier-depot.ch/mein-konto/vorbestellungen/" className='h-72 w-72' />
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className={'p-4'}>
                     <Button type="primary" onClick={onClose}>Schliessen</Button>
